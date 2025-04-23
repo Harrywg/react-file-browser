9e73b920a01212dca26c0991f8f01dda774f583a
Harrywg <wardgray24@gmail.com>
2025-04-23 21:34:35 +0100

    chore: manual prettier formatting

--- src/lib/hooks/useFilesQuery.ts
+++ src/lib/hooks/useFilesQuery.ts
@@ -2,15 +2,15 @@ import { useQuery } from '@tanstack/react-query';
 import { getFiles } from '@/lib/api';
 
 export default function useFilesQuery() {
-   const { data: files = [], isLoading } = useQuery({
-      queryKey: ['files'],
-      queryFn: getFiles,
-      staleTime: 5 * 60 * 1000,
-      refetchOnWindowFocus: false,
-   });
+    const { data: files = [], isLoading } = useQuery({
+        queryKey: ['files'],
+        queryFn: getFiles,
+        staleTime: 5 * 60 * 1000,
+        refetchOnWindowFocus: false,
+    });
 
-   return {
-      files,
-      isLoading,
-   };
+    return {
+        files,
+        isLoading,
+    };
 }
