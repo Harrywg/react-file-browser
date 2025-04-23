ccb180b71ae4a28c6a58f5d9cfbfa7b69ad22dfb
Harrywg <wardgray24@gmail.com>
2025-04-23 21:32:55 +0100

    chore: run format for tw

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
    return (
       <header>
-         <h1 className="font-bold text-3xl">File Browser</h1>
+         <h1 className="text-3xl font-bold">File Browser</h1>
       </header>
    );
 }
