7b8d1cec1c2dfa3e49cac2659b6f9f4c987076c4
Harrywg <wardgray24@gmail.com>
2025-04-23 20:51:05 +0100

    initial project boilerplate

---
+++ jest.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+    verbose: true,
+    preset: 'ts-jest',
+    testEnvironment: 'jsdom',
+    transform: {
+        '^.+\\.tsx?$': ['ts-jest', {
+            tsconfig: 'tsconfig.test.json'
+        }]
+    },
+    moduleNameMapper: {
+        '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
+    },
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts']
+};
+
+export default config;
